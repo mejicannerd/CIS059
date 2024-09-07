8722688db6c5693f532a65db752fc796edfa1188
Daniel Gonzalez <102626387+mejicannerd@users.noreply.github.com>
2024-09-07 16:05:11 -0700

    African Big Cats Example Code Setup
    Recreated the initial setup of the African Big Cats project. Successfully executed code after initial setup.

--- africanbigcats/src/main/java/com/africanbigcats/App.java
+++ africanbigcats/src/main/java/com/africanbigcats/App.java
@@ -1,13 +1,32 @@
 package com.africanbigcats;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        Menu appMenu = new Menu();
+
+        Character command = '_';
+        LinkedList<Panthera> catList = new LinkedList<>();
+
+        // loop until user quits
+        while (command != 'q') {
+
+            // print the menu
+            appMenu.print();
+
+            // get a command
+            System.out.print("Enter a command: ");
+            command = appMenu.getCommand();
+
+            // execute a command
+            appMenu.executeCommand(command, catList);
+
+            // move the cats to a new position in africa
+            appMenu.update(catList);
+
+        }
+
     }
 }
